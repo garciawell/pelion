7d4ce417b7aa24e17827cff4abfc45f8df7c4a2d
Wellington <wgarcia@inundaweb.com.br>
2017-10-30 22:17:28 -0200

    Estilizando Grid posts

--- wp-content/themes/pelion/templates/loop-post.php
+++ wp-content/themes/pelion/templates/loop-post.php
@@ -1,4 +1,4 @@
-<div class="col-4 col-lg-4 col-md-6 col-sm-12">
+<div class="col-4 col-lg-4 col-md-6 col-sm-6">
 
 	<div class="bloco-padrao">
 		<figure><?php the_post_thumbnail( 'large', array( 'alt' => get_the_title() ) );  ?></figure>
