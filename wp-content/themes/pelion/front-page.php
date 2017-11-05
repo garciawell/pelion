9788d1627bedd721bcedd9483a5d15dc908c00da
Wellington <wgarcia@inundaweb.com.br>
2017-11-05 05:20:51 -0200

    Home

--- wp-content/themes/pelion/front-page.php
+++ wp-content/themes/pelion/front-page.php
@@ -1,36 +1,100 @@
 <?php get_header(); /* Template Name: Home*/ ?>
-<div class="slider container-full">
-	<?php echo do_shortcode('[rev_slider alias="home"]');?>
+<section class="slider">
+   <?php echo do_shortcode('[rev_slider alias="home"]');?>     
+</section>
+
+
+<section class="inf-home-tp container-full">
+   <div class="container">
+      <?php the_content(); ?>
+   </div>
+</section>
+
+<div class="bloco-grid">
+   <?php 
+         // check if the flexible content field has rows of data
+   if( have_rows('items')  ): ?>
+      <?php // loop through the rows of data
+      while ( have_rows('items') ) : the_row();
+
+        if( get_row_layout() == 'template_packages' ): ?>
+        <section class="container-full grey">
+         <div class="container">
+            <div class="full-cat-front">
+               <div class="row aling-items-center header-cat">
+                  <div class="col">
+                     <h3><?php the_sub_field('title'); ?></h3>
+                  </div>
+                  <div class="col">
+                     <a href="<?php the_sub_field('link'); ?>" class="pull-right btn btn-outline-success">SEE ALL</a>
+                  </div>
+               </div>
+               <div class="row loop-items-in"><?php
+               $posts = get_sub_field('blocks');
+
+               if( $posts ): ?>
+               <?php  foreach( $posts as $post):  ?>
+                  <?php   get_template_part("templates/loop", "post-4"); ?>
+               <?php endforeach; ?>
+            <?php  endif; wp_reset_postdata();   ?>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<?php elseif( get_row_layout() == 'template_regions' ):  ?>  
+  <section class="container-full grey">
+      <div class="container">
+
+         <div class="full-reg-front">
+            <div class="row aling-items-center header-cat">
+               <div class="col">
+                  <h3><?php the_sub_field('title'); ?></h3>
+               </div>
+               <div class="col">
+                  <a href="<?php the_sub_field('link'); ?>" class="pull-right btn btn-outline-success">SEE ALL</a>
+               </div>
+            </div>
+
+
+               <?php 
+
+               $terms = get_sub_field('blocks');
+
+               if( $terms ): ?>
+
+                  <div class="row">
+
+                  <?php foreach( $terms as $term ): ?>
+
+                     <div class="col-12 col-md-3"">
+                        <div class="col-in" style="background-image: url('<?php  echo $custom_field = get_field('imagem_categoria', $term ); ?>');">
+                        
+                              <span class="subtitle-cat"><?php the_field('subtitle', $term ); ?></span>
+
+                              <h2 class="title-cat-pag"><?php echo  $term->name; ?></h2>
+                        
+                        </div>
+                     </div>
+
+                  <?php endforeach; ?>
+
+                  </div>
+
+               <?php endif; ?>
+
+
+         </div> 
+      </div>
+   </section>
+
+<?php  endif; ?>
+
+
+<?php  endwhile; endif; ?>
+
+
 </div>
 
-<div class="container-full">
-	<div id="content-main">
-		<div class="container">
-			<div class="row">
-
-
-				<?php 
-				$new_query = new WP_Query( array(
-					'posts_per_page' => 14,
-					'post_type'      => packages,
-					'orderby'        => 'menu_order'
-				   // 'paged'          => $paged
-				) );
-
-				while ( $new_query->have_posts() ) : $new_query->the_post();  ?>
-
-						<?php   get_template_part("templates/loop", "post-4"); ?>
-
-			<?php endwhile;  
-			wp_reset_postdata();
-
-			?>
-
-
-
-		</div>
-	</div>
-</div>
-</div>
-
-<?php get_footer (); ?>  	 
+<?php get_footer (); ?>
