f793563a2b9d547fe5235d6e09d608814c69776c
Wellington <wgarcia@inundaweb.com.br>
2017-12-04 01:39:39 -0200

    Page Search

--- wp-content/themes/pelion/search.php
+++ wp-content/themes/pelion/search.php
@@ -10,10 +10,10 @@
 get_header(); ?>
 <div class="banner-full d-flex  text-center align-items-center" style="background-size:cover; background-image: url('<?php  if( get_field('banner') ): the_field('banner'); else: echo bloginfo(home) . '/wp-content/uploads/2017/10/banner-regioes.jpg';   endif;; ?>');">
 
-   <div class="container">
-      <h1 class="title-cat text-center">Search</h1>
+	<div class="container">
+		<h1 class="title-cat text-center">Search</h1>
 
-   </div>
+	</div>
 </div>
 
 <section id="primary" class="content-area-mg">
@@ -21,45 +21,58 @@ get_header(); ?>
 		<div id="content-main"> 
 			<div class="container"> 
 
-				<?php if ( have_posts() ) : ?>
-
-					<header class="page-header">
-						<h1 class="title-main"><?php printf( __( 'You searched for: %s' ), '<span class="focus-search">' .get_search_query() ).'</span>' ?></h1> 
-					</header><!-- .page-header -->
-					<div class="row">
-						<?php
-					// Start the loop.
-						while ( have_posts() ) : the_post();
-
-							get_template_part("templates/loop", "post-4"); ?>
+				<div class="row-pad-25 row order-resp">
+					<div class="pad-25 col-12 col-lg-9  col-sm-12 content-filter">
+						<div id="main">
 
 
-					<?php // End the loop.
-				endwhile;
+							<header class="page-header">
+							<h1 class="title-main"><?php printf( __( 'You searched for: %s' ), '<span class="focus-search">' .get_search_query() ).'</span>' ?></h1> 
+							</header><!-- .page-header -->
+							<div class="row">
+								<?php if ( have_posts() ) : ?>
+								<?php
+								// Start the loop.
+								while ( have_posts() ) : the_post();
+
+								get_template_part("templates/loop", "post"); ?>
 
 
-				// If no content, include the "No posts found" template.
-			else :
-				echo "<h2 class='text-center'>No results found for: " .get_search_query() . "</h2>"; ?>
-				<form role="search" method="get" class="search-form-line" action="<?php echo home_url( '/' ); ?>">
-					<label>
-						<input type="search" class="search-field"
-						placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
-						value="<?php echo get_search_query() ?>" name="s"
-						title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-					</label>
-					<input type="submit" class="search-submit"
-					value="OK" />
-				</form>
+								<?php // End the loop.
+								endwhile;
+								// If no content, include the "No posts found" template.
+								else :
+
+								echo "<h2 class='col-12 '>No results found for: " .get_search_query() . "</h2>"; ?>
+								<form role="search" style="float: left;
+    margin-left: 0;" method="get" class="search-form-line" action="<?php echo home_url( '/' ); ?>">
+									<label>
+										<input type="search" class="search-field"
+										placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+										value="<?php echo get_search_query() ?>" name="s"
+										title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+									</label>
+									<input type="submit" class="search-submit"
+									value="OK" />
+								</form>
 
 
-			<?php endif;
-			?>
+								<?php endif;
+								?>
+							</div>
+						</div>
+					</div>
+					<div class="pad-25 col-12 col-lg-3  col-sm-12 item-filter">
+						<div class="filtro item-sidebar">
+							<h3 class="title-cat-side">
+							Filter</h3>
+							<?php echo do_shortcode('[searchandfilter id="63"]'); ?>
+						</div>
+					</div>
+			</div>
 		</div>
-
 	</div>
-</div>
-</main><!-- .site-main -->
+	</main><!-- .site-main -->
 </section><!-- .content-area -->
 
 <?php get_footer(); ?>
