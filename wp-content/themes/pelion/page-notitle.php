0f6adc4c5e6a9ed41a33edf66417160f9d64065d
Wellington <wgarcia@inundaweb.com.br>
2017-12-04 00:23:36 -0200

    Topo Paginas Banner

--- wp-content/themes/pelion/page-notitle.php
+++ wp-content/themes/pelion/page-notitle.php
@@ -1,7 +1,7 @@
 <?php get_header(); /* Template Name: Page Banner*/?>
 
 
-<div class="banner-full d-flex  text-center align-items-center" style="background-size:cover; background-image: url('<?php  if( get_field('banner') ): the_field('large_category'); else: echo bloginfo(home) . '/wp-content/uploads/2017/10/banner-regioes.jpg';   endif;; ?>');">
+<div class="banner-full d-flex  text-center align-items-center" style="background-size:cover; background-image: url('<?php  if( get_field('banner') ): the_field('banner'); else: echo bloginfo(home) . '/wp-content/uploads/2017/10/banner-regioes.jpg';   endif;; ?>');">
 	<div class="container">
 		<h1 class="title-white"><?php the_title(); ?></h1>
 		<a class="ancora"  href="#content-main"><img src="<?php bloginfo('template_url'); ?>/img/icon-down.png"></a>	
--- wp-content/themes/pelion/search.php
+++ wp-content/themes/pelion/search.php
@@ -8,7 +8,7 @@
  */
 
 get_header(); ?>
-<div class="banner-full d-flex  text-center align-items-center" style="background-size:cover; background-image: url('<?php  if( get_field('banner') ): the_field('large_category'); else: echo bloginfo(home) . '/wp-content/uploads/2017/10/banner-regioes.jpg';   endif;; ?>');">
+<div class="banner-full d-flex  text-center align-items-center" style="background-size:cover; background-image: url('<?php  if( get_field('banner') ): the_field('banner'); else: echo bloginfo(home) . '/wp-content/uploads/2017/10/banner-regioes.jpg';   endif;; ?>');">
 
    <div class="container">
       <h1 class="title-cat text-center">Search</h1>
